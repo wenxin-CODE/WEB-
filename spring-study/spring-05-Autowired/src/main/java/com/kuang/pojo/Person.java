6aa4a9e4ad10b8519abdd94926f613c8b56a644d
dly <2011429436@qq.com>
2021-06-08 00:32:11 +0800

    vue学习

---
+++ spring-study/spring-05-Autowired/src/main/java/com/kuang/pojo/Person.java
@@ -0,0 +1,44 @@
+package com.kuang.pojo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Person {
+    @Autowired
+    private Dog dog;
+    @Autowired
+    private Cat cat;
+    private String name;
+
+    public Dog getDog() {
+        return dog;
+    }
+
+    public void setDog(Dog dog) {
+        this.dog = dog;
+    }
+
+    public Cat getCat() {
+        return cat;
+    }
+
+    public void setCat(Cat cat) {
+        this.cat = cat;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "dog=" + dog +
+                ", cat=" + cat +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
