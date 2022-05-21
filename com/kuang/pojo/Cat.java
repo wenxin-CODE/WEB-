11debd826849dc2c078bfb63e326c9f16f29062d
‘mike’ <mike_s@163.com>
2022-05-21 16:48:46 +0800

    delete

--- com/kuang/pojo/Cat.java
+++
@@ -1,7 +0,0 @@
-package com.kuang.pojo;
-
-public class Cat {
-    public void shout(){
-        System.out.println("miao");
-    }
-}
--- com/kuang/pojo/Dog.java
+++
@@ -1,7 +0,0 @@
-package com.kuang.pojo;
-
-public class Dog {
-    public void shout(){
-        System.out.println("wang");
-    }
-}
